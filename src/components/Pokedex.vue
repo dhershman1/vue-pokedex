18a6959a6964a1180caeac528221c2d69b53f886
dhershman1 <dustinh17@gmail.com>
2024-01-26 22:53:31 -0500

    Remove unused computed

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { onMounted, ref, computed } from 'vue'
+  import { onMounted, ref } from 'vue'
   import { capitalize } from 'kyanite'
   import { usePokemonStore } from '../stores/pokemon'
   import Card from './Card.vue'
